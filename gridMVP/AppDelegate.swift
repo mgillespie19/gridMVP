f651ca09e1b40709a18f70f8721bf5f5c40f6d74
Max Gilespie <max.gillespie10@gmail.com>
2019-09-13 12:25:35 -0500

    add placeholder rings

--- gridMVP/AppDelegate.swift
+++ gridMVP/AppDelegate.swift
@@ -16,7 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        UITabBar.appearance().tintColor = .black
+        UITabBar.appearance().backgroundColor = .black
+        
+        
         return true
     }
 
