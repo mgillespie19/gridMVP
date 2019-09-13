fe686f6ac94263f322c6b4239b5c875d5cdb1e26
Max Gilespie <max.gillespie10@gmail.com>
2019-09-13 16:32:41 -0500

    add map, set center at pittsburgh

--- gridMVP/Controller/DiscoverViewController.swift
+++ gridMVP/Controller/DiscoverViewController.swift
@@ -8,25 +8,29 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 class DiscoverViewController: UIViewController {
     
-    @IBOutlet weak var ScrollView: UIScrollView!
+    @IBOutlet weak var MapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        ScrollView.delegate = self
-        navigationController?.setNavigationBarHidden(true, animated: false)
+        self.mapLocationUpdate(latitude: 40.4406, longitude: -79.9959, title: "")
     }
     
-}
-
-extension DiscoverViewController: UIScrollViewDelegate {
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        
-        let offsetY = scrollView.contentOffset.y
-        let navigationIsHidden = offsetY <= 0
-        navigationController?.setNavigationBarHidden(navigationIsHidden, animated: true)
+    func mapLocationUpdate(latitude: Double, longitude: Double, title: String) {
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        self.centerMapOnLocation(location: location, title: title)
     }
+    
+    func centerMapOnLocation(location: CLLocationCoordinate2D, title: String) {
+        let coordinateRegion = MKCoordinateRegion(center: location, span: MKCoordinateSpan(latitudeDelta: 0.15, longitudeDelta: 0.15))
+        self.MapView.setRegion(coordinateRegion, animated: false)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = title
+        self.MapView.addAnnotation(annotation)
+    }
+    
 }
